32407f9f600db8f8d86f82b8973e5befbc729eeb
kirmaniarslan <arslankirmani9600@gmail.com>
2016-04-22 22:30:26 +0500

    Fix Color
    - Use rand Function

--- circle.cpp
+++ circle.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 #define PI 3.14159
 
-void drawCircLe( int x, int y, int radius, int color );
+void drawCircLe( int x, int y, int radius );
 
 struct Circle {
 
 	int x;
 	int y;
-	int color;
+
 	int radius;
 
 };
@@ -25,8 +25,8 @@ int main( )
 	c.x = 250;
 	c.y = 250;
 	c.radius = 100;
-	c.color = 0;
-	drawCircLe( c.x, c.y, c.radius, c.color);
+
+	drawCircLe( c.x, c.y, c.radius);
 
 	while (!kbhit( ))
 	{
@@ -38,7 +38,7 @@ int main( )
 
 }
 
-void drawCircLe( int x, int y, int radius, int color )
+void drawCircLe( int x, int y, int radius)
 {
 	
 	// Base case.
@@ -46,16 +46,18 @@ void drawCircLe( int x, int y, int radius, int color )
 		return;
 
 	// Draw circle
-	setcolor(color);
-	circle(x,y,radius);
+	int color = rand( ) % 15 + 2;
+	setcolor( color );
+	circle( x, y, radius );
 
 	int newRadius = 0.51*radius;
 
 	// Recursive calls
+	// Calculating Cartesian coordinates from Polar coordinates 
 	for(int i = 1; i <= 4; i++)
 	{
 
-		drawCircLe( x + radius * cos( i * PI / 2 ), y + radius * sin( i * PI / 2 ), newRadius , color++ );
+		drawCircLe( x + radius * cos( i * PI / 2 ), y + radius * sin( i * PI / 2 ), newRadius);
 
 	}
 
